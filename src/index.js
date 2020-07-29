cf4fcc1ccd755a40f921bfb313a21955c4240289
jean-paulo <jeanprs@msn.com>
2020-07-29 10:36:20 -0300

    Cadastro e Cadastro de categoria

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from '../src/pages/Home';
+import {BrowserRouter,  Switch, Route} from 'react-router-dom';
+import CadastroVideo from '../src/pages/Cadastro/Video';
+import CadastroCategoria from '../src/pages/Cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={Home} exact/>
+      <Route path='/cadastro/video' component={CadastroVideo} />
+      <Route path='/cadastro/categoria' component={CadastroCategoria} />
+      <Route component={()=> (
+        <div>404 Not Found</div>
+      )}/>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
